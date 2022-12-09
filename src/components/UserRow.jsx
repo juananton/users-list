c8089848906092ce44b6416f54077bf4bb63a2b0
juananton <juananton.correo@gmail.com>
2022-12-09 15:38:16 +0100

    Status and role conditional rendering

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,9 +1,31 @@
-const UserRow = ({ name, active, role, ...props }) => (
-	<div className='user' {...props}>
-		<span className='name'>{name}</span>
-		<span className='active'>{active}</span>
-		<span className='role'>{role}</span>
-	</div>
-);
+import style from './UserRow.module.css';
+
+const UserRow = ({ name, active, role, ...props }) => {
+	const activeClassName = active ? style.active : style.inactive;
+
+	const ROLE_STYLES = {
+		teacher: ['Profesor', style.teacher],
+		student: ['Alumno', style.student],
+		other: ['Otro', null]
+	};
+
+	const [roleName, roleClassName] = ROLE_STYLES[role] || ROLE_STYLES.other;
+
+	return (
+		<div className={style.wrapper} {...props}>
+			<div className={style.name}>
+				<span>{name}</span>
+			</div>
+			<div className={style.status}>
+				<span className={activeClassName}>
+					{active ? 'Activo' : 'Inactivo'}
+				</span>
+			</div>
+			<div className={style.role}>
+				<span className={`${style.tag} ${roleClassName}`}>{roleName}</span>
+			</div>
+		</div>
+	);
+};
 
 export default UserRow;
