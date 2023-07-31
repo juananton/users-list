b77de04b42f7556c37f6ee86778d988fd94d7ea1
juananton <juananton.correo@gmail.com>
2023-07-31 15:36:16 +0200

    Create search in list component

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import UserRow from './UserRow';
 import style from './UsersList.module.css';
 
 const UsersList = ({ users }) => {
+  const [search, setSearch] = useState('');
+  const normalizeSearch = search.toLowerCase();
+
+  const usersFiltered = search
+    ? users.filter(user => user.name.toLowerCase().startsWith(normalizeSearch))
+    : users;
+
   const usersRendered =
-    users.length > 0 ? (
-      users.map(user => <UserRow key={user.name} {...user} />)
+    usersFiltered.length > 0 ? (
+      usersFiltered.map(user => <UserRow key={user.name} {...user} />)
     ) : (
       <p>No hay usuarios</p>
     );
   return (
     <div className={style.list}>
       <h1>Listado de usuarios</h1>
+
+      <input
+        type='text'
+        name='search'
+        value={search}
+        onChange={ev => setSearch(ev.target.value)}
+      />
       {usersRendered}
     </div>
   );
