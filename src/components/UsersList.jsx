0412f7578a7b3887e1680e87c1f82f1d4f878699
juananton <juananton.correo@gmail.com>
2023-07-31 20:17:21 +0200

    Create filter by active and sort

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,19 +4,39 @@ import style from './UsersList.module.css';
 
 const UsersList = ({ users }) => {
   const [search, setSearch] = useState('');
+  const [onlyActive, setOnlyActive] = useState(false);
+  const [sortBy, setSortBy] = useState(0);
 
-  const usersFiltered = filterUsersByName(users, search);
+  let usersFiltered = filterActiveUsers(users, onlyActive);
+  usersFiltered = filterUsersByName(usersFiltered, search);
+  usersFiltered = sortUsers(usersFiltered, sortBy);
   const usersRendered = RenderUsers(usersFiltered);
 
   return (
-    <div className={style.list}>
+    <div className={style.wrapper}>
       <h1>Listado de usuarios</h1>
-      <input
-        type='text'
-        name='search'
-        value={search}
-        onChange={ev => setSearch(ev.target.value)}
-      />
+      <form className={style.form}>
+        <input
+          type='text'
+          value={search}
+          onChange={ev => setSearch(ev.target.value)}
+        />
+        <div className={style.active}>
+          <input
+            type='checkbox'
+            checked={onlyActive}
+            onChange={ev => setOnlyActive(ev.target.checked)}
+          />
+          <span>Sólo activos</span>
+        </div>
+        <select
+          value={sortBy}
+          onChange={ev => setSortBy(Number(ev.target.value))}
+        >
+          <option value={0}>Por defecto</option>
+          <option value={1}>Por nombre</option>
+        </select>
+      </form>
       {usersRendered}
     </div>
   );
@@ -30,6 +50,25 @@ const filterUsersByName = (users, search) => {
   return users.filter(user =>
     user.name.toLowerCase().startsWith(lowerCasedSearch)
   );
+};
+
+const filterActiveUsers = (users, active) => {
+  if (!active) return users;
+
+  return users.filter(user => user.active);
+};
+
+const sortUsers = (users, sortBy) => {
+  switch (sortBy) {
+    case 1:
+      return users.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return 0;
+      });
+    default:
+      return users;
+  }
 };
 
 const RenderUsers = users => {
