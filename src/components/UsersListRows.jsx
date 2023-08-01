72cd0e8fcfc493a578e53afe28d239a8e264569e
juananton <juananton.correo@gmail.com>
2023-08-01 18:37:41 +0200

    Create usersContext

--- src/components/UsersListRows.jsx
+++ src/components/UsersListRows.jsx
@@ -1,10 +1,8 @@
 import UserRow from './UserRow';
 
-const UsersListRows = ({ users, toggleUserActive }) => {
+const UsersListRows = ({ users }) => {
   if (!users.length) return <p>No hay usuarios</p>;
-  return users.map(user => (
-    <UserRow key={user.id} toggleUserActive={toggleUserActive} {...user} />
-  ));
+  return users.map(user => <UserRow key={user.id} {...user} />);
 };
 
 export default UsersListRows;
