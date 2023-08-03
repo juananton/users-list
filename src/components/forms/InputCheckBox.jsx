c9656ac6a422e6155f477b81f5510b5be47555a9
juananton <juananton.correo@gmail.com>
2023-08-03 14:52:41 +0200

    Create InputCheckBox component

---
+++ src/components/forms/InputCheckBox.jsx
@@ -0,0 +1,11 @@
+import CheckIcon from '../icons/CheckIcon';
+import style from './InputCheckbox.module.css';
+
+const InputCheckBox = ({ className, ...props }) => (
+  <label className={`${style.label} ${className || ''}`}>
+    <input {...props} type='checkbox' className={style.input} />
+    <CheckIcon className={style.check} />
+  </label>
+);
+
+export default InputCheckBox;
